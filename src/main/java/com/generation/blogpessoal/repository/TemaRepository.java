b965f218cbe58e14a466651777e017fdc633d785
Jaiane Oliveira <blue.love02c@gmail.com>
2022-03-29 18:09:11 -0300

    Atividade #11 - CRUD 3

---
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,20 @@
+package com.generation.blogpessoal.repository;
+
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoal.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+    
+    public List <Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+    
+    public Object existsById(@Valid Tema tema);
+
+}
